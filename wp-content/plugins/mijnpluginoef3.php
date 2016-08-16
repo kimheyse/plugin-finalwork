e46c9df2456f7b8e4b75469464a55b64a1a01a0b
Kim Heyse <kim.hey@hotmail.com>
2016-08-16 21:56:30 +0200

    Adding code to my plugin

--- wp-content/plugins/mijnpluginoef3.php
+++ wp-content/plugins/mijnpluginoef3.php
@@ -1,2 +1,19 @@
 <?php
-// Silence is golden.
+/**
+* Plugin Name: mijnplugin
+* Description: Some information about my plugin
+* Author: Kim Heyse
+*/
+
+/* tutorial: http://wpninjas.com/how-to-create-a-simple-wordpress-plugin/ */
+
+function my_custom_redirect () {
+	global $post;
+	if ( is_page() || is_object( $post ) ) {
+		if ( $redirect = get_post_meta($post-&gt;ID, 'redirect', true ) ) {
+                        wp_redirect( $redirect );
+                        exit;
+                }
+        }
+}
+add_action( 'get_header', 'my_custom_redirect' );
